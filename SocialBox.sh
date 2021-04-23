7312893a282a7b5c17ad9ae024e112e3508682af
MyeRs <51493986+GamePowerST@users.noreply.github.com>
2021-04-23 19:10:17 +0300

    Update SocialBox.sh

--- SocialBox.sh
+++ SocialBox.sh
@@ -14,14 +14,14 @@ blue="\033[1;34m"
 nc="\e[0m"
 #ROOT PRIVILEGIES
 #if [[ $EUID -ne 0 ]]; then
-       echo -e "$red by samsesh.$nc"
+       echo -e "$red by MyeRs.$nc"
 #        exit 1
 #fi
 #Installation
 sleep 1
-echo -e "Checking Installation $nc"
+echo -e "Kurulum Kontrol Ediliyor $nc"
 bash install-sb.sh
-echo -e "Checking Completed [$green✓$nc] $nc "
+echo -e "Kontrol tamamlandı [$green✓$nc] $nc "
 sleep 1
 clear
 #Startup
@@ -31,30 +31,30 @@ echo "		▐█ ▀. ▪     ▐█ ▌▪██ ▐█ ▀█ ██•      ▐
 echo "		▄▀▀▀█▄ ▄█▀▄ ██ ▄▄▐█·▄█▀▀█ ██▪      ▐█▀▀█▄ ▄█▀▄  ·██· "
 echo "		▐█▄▪▐█▐█▌.▐▌▐███▌▐█▌▐█ ▪▐▌▐█▌▐▌    ██▄▪▐█▐█▌.▐▌▪▐█·█▌"
 echo -e "		 ▀▀▀▀  ▀█▄▀▪·▀▀▀ ▀▀▀ ▀  ▀ .▀▀▀     ·▀▀▀▀  ▀█▄▀▪•▀▀ ▀▀$nc $blue v1.beta$nc"
-#echo -e "	[+]              $red Coded By sam.sesh$nc         		   [+]"
-#echo -e "	[+] 		 $red www.twitter.com/init__0$nc 		   [+]"
-#echo -e "	[+] 		 $red Greetz To All Pentesters$nc          		   [+]"
+#echo -e "	[+]              $red Coded By MyeRs$nc         		   [+]"
+#echo -e "	[+] 		 $red www.instagram.com/mrcclk_$nc 		   [+]"
+#echo -e "	[+] 		 $red Sosyal Medya Atağı$nc          		   [+]"
 #echo -e "	[+]$red Special Greetz To$nc :$green {thelinuxchoice , Ha3MrX, Tunisian Eagles, init__0}$nc [+]"
 echo ""
-echo -e "$yellow Select From Menu : $nc"
+echo -e "$yellow Menüden Seçim Yapın : $nc"
 echo ""
-echo -e "		$Cyan 1 : Brute Force Facebook Account$nc"
-echo -e "		$Cyan 2 : Brute Force Gmail Account$nc"
-echo -e "		$Cyan 3 : Brute Force Instagram Account$nc"
-echo -e "		$Cyan 4 : Brute Force Twitter Account$nc"
-echo -e "		$Cyan 99: Exit$nc"
+echo -e "		$Cyan 1 : Facebook Hesabı Kaba Kuvvet$nc"
+echo -e "		$Cyan 2 : Gmail Hesabı Kava Kuvvet$nc"
+echo -e "		$Cyan 3 : İnstagram Hesabı Kaba Kuvvet$nc"
+echo -e "		$Cyan 4 : Twitter Hesabı Kaba Kuvvet$nc"
+echo -e "		$Cyan 99: Çıkış$nc"
 read -p "Choice >  " ch
 if [ $ch = 1 ]; then
-echo -e "			$Cyan Facebook Brute Force$nc"
+echo -e "			$Cyan Facebook Kaba Kuvvet$nc"
 echo -e "$green"
-read -p "Enter Facebook ID / Email / Username / Number: " id
-read -p "Enter wordlist path : " wordlist
+read -p "Gir Facebook ID / Email / Kullanıcı İsmi / Numara: " id
+read -p "Wordlist dosya konumu : " wordlist
 echo -e "$nc"
 cd facebook
 perl fb-brute.pl $id $wordlist
-echo -e " 		[+]$yellow Brute Force Complete $nc[$green✓$nc] $nc[+]"
+echo -e " 		[+]$yellow Kaba Kuvvet Tamamlandı $nc[$green✓$nc] $nc[+]"
 echo -e "$red"
-read -p "Wanna Back To Main Menu [ Y / n ] : " check
+read -p "Ana Menüye Geri Dönmek İstiyormusun [ Y / n ] : " check
 echo -e "$nc"
 if [ $check = "Y" ]; then
 cd .. && bash SocialBox.sh
@@ -70,12 +70,12 @@ else
 exit 1
 fi
 elif [ $ch = 2 ]; then
-echo -e "			$Cyan Gmail Brute Force$nc"
+echo -e "			$Cyan Gmail Kaba Kuvvet$nc"
 cd Gemail-Hack
 python gemailhack.py
-echo -e "               [+]$yellow Brute Force Complete $nc[$green✓$nc] $nc[+]"
+echo -e "               [+]$yellow Kaba Kuvvet Tamamlandı $nc[$green✓$nc] $nc[+]"
 echo -e "$red"
-read -p "Wanna Back To Main Menu [ Y / n ] : " check2
+read -p "Ana Menüye Geri Dönmek İstiyormusun [ Y / n ] : " check2
 echo -e "$nc"
 if [ $check2 = "Y" ]; then
 cd .. && bash SocialBox.sh
@@ -91,18 +91,18 @@ else
 exit 1
 fi
 elif [ $ch = 3 ]; then
-echo -e "			$Cyan Instagram Brute Force$nc"
+echo -e "			$Cyan Instagram Kaba Kuvvet$nc"
 cd instainsane/
 sleep 0.025
-	echo -e "	[+]$red tor $nc[+]"
+	echo -e "	[+]$red Tor Servisi Başlatılıyor $nc[+]"
 sleep 0.9
 service tor start
 ./instainsane.sh
-echo -e "               [+]$yellow Brute Force Complete $nc[$green✓$nc] $nc[+]"
+echo -e "               [+]$yellow Kaba Kuvvet Tamamlandı $nc[$green✓$nc] $nc[+]"
 service tor stop
 #echo -e "		[+]$red Service Tor Stopped$nc [+]"
 echo -e "$red"
-read -p "Wanna Back To Main Menu [ Y / n ] : " check3
+read -p "Ana Menüye Geri Dönmek İstiyormusun [ Y / n ] : " check3
 echo -e "$nc"
 if [ $check3 = "Y" ]; then
 cd .. && bash SocialBox.sh
@@ -118,18 +118,18 @@ else
 exit 1
 fi
 elif [ $ch = 4 ]; then
-echo -e "			$Cyan Twitter Brute Force$nc"
+echo -e "			$Cyan Twitter Kaba Kuvvet$nc"
 cd tweetshell/
 sleep 0.025
-        echo -e "	[+]$red Service Tor Stated $nc[+]"
+        echo -e "	[+]$red Tor Servisi Başlatılıyor $nc[+]"
 sleep 0.9
 service tor start
 bash tweetshell.sh
-echo -e "               [+]$yellow Brute Force Complete $nc[$green✓$nc] $nc[+]"
+echo -e "               [+]$yellow Kaba Kuvvet Tamamlandı $nc[$green✓$nc] $nc[+]"
 service tor stop
-echo -e "		[+]$red Service Tor Stopped$nc [+]"
+echo -e "		[+]$red Tor Servisi Durduruluyor$nc [+]"
 echo -e "$red"
-read -p "Wanna Back To Main Menu [ Y / n ] : " check4
+read -p "Ana Menüye Geri Dönmek İstiyormusun [ Y / n ] : " check4
 echo -e "$nc"
 if [ $check4 = "Y" ]; then
 cd .. && bash SocialBox.sh
@@ -145,10 +145,10 @@ else
 exit 1
 fi
 elif [ $ch == 99 ]; then
-echo -e "$red Program Exit ...$nc"
+echo -e "$red Programdan Çıkış Yapılıyor...$nc"
 sleep 0.25
 exit 1
 else
-echo "Not Found 404 , Exit"
+echo "404 Bulunamadı , Çıkış"
 exit 1
 fi
